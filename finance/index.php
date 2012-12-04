4831a5d74beba57b528e55cd8944fcf08178e527
root <root@keeper1.(none)>
2012-12-05 03:11:36 +0400

    new .gitignore file

--- finance/index.php
+++ finance/index.php
@@ -5,19 +5,20 @@
 Read,learn and share this
 </p>
 <br />
-<a href="http://docs.google.com/gview?url=gauravbits.com/finance/powerpoint-presentation-investment-basics2732.ppt">powerpoint-presentation-investment-basics2732.ppt</a>
+<a href="http://docs.google.com/gview?url=gauravbits.com/finance/powerpoint-presentation-investment-basics2732.ppt">powerpoint-presentation-investment-basics2732.ppt</a><br /><br />
+<a href="http://www.youtube.com/watch?v=HkD2JO0ZgRM" target="_blank">Credit CRISIS Animation Sub Prime just PRETEXT Banks Brokers Mortgages Foreclosure Money and Debt</a>
 	</div>
 
 <div id="content-right">
 <?php
-/*$url='http://www.moneycontrol.com/terminal/';
+$url='http://www.moneycontrol.com/terminal/';
 $ch = curl_init();
 $timeout = 50;
 curl_setopt($ch,CURLOPT_URL,$url);
 curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
 curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,$timeout);
 $data = curl_exec($ch);
-$part = explode('sensex</b>',$data,2) ;
+$part = explode('Sensex</b>',$data,6) ;
 //echo $part[0];
 $val = explode('</div>',$part[1]);
 
@@ -26,7 +27,7 @@ echo $val[0] ;
 //$val = explode('',$part[68]);
 //echo $val[1] ;
 curl_close($ch);
-*/?>
+?>
 </div>
 </div>
 </div>
--- index.php
+++ index.php
@@ -3,55 +3,16 @@
     <div id="content"> 
 	<div id="content-left">
     <p> 
-    Hi all, This is Gaurav Kumar. I just completed final year, M.Sc(tech) Information Systems from BITS pilani.
+    Hi all, This is Gaurav Kumar. I have completed M.Sc(tech) Information Systems from BITS pilani(2008-12). I am currently working in a startup and I am interested mostly in technology and finance.
     </p>
-    
+  <img src="qr.jpg"></a>
     </div>
     <div id="content-right">
 <div id="contenta">
 <div id="content-lefta">
-<img src="qr.jpg"></a>
 <?php
 //include "qrlib.php";
 //QRcode::png("Gaurav 8745932313");
-/*    $url='http://www.moneycontrol.com/sensex/bse/sensex-live';
-$ch = curl_init();
-$timeout = 50;
-curl_setopt($ch,CURLOPT_URL,$url);
-curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
-curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,$timeout);
-$data = curl_exec($ch);
-$part = explode('</div>',$data,69) ;
-$val = explode('<div>',$part[67]);
-
-echo "<html><body> SENSEX <br/></body> </html>" ;
-echo $val[0] ;
-$val = explode('<p',$part[68]);
-echo $val[0] ;
-curl_close($ch);
-?>
-</div>
-</div>
-<div id="content-righta">
-<?php
-$url='http://www.moneycontrol.com/nifty/nse/nifty-live';
-$ch = curl_init();
-$timeout = 50;
-curl_setopt($ch,CURLOPT_URL,$url);
-curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
-curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,$timeout);
-$data = curl_exec($ch);
-$part = explode('</div>',$data,69) ;
-$val = explode('<div>',$part[67]);
-
-echo "<html><body>NIFTY<br/> </body> </html>" ;
-echo $val[0] ;
-$val = explode('<p',$part[68]);
-echo $val[0] ;
-
-
-curl_close($ch);
-*/
 ?>
     </div>  
 	</div> 
